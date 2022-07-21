173f7e144009f198dad5811697285d732ac62a97
funzter <cavehive@protonmail.com>
2022-07-21 14:12:57 +0200

    init

---
+++ src/main/java/de/timesnake/library/network/ServerCreationResult.java
@@ -0,0 +1,44 @@
+package de.timesnake.library.network;
+
+import java.nio.file.Path;
+
+public interface ServerCreationResult {
+
+    boolean isSuccessful();
+
+    class Successful implements ServerCreationResult {
+
+        private final Path serverPath;
+
+        public Successful(Path serverPath) {
+            this.serverPath = serverPath;
+        }
+
+        public Path getServerPath() {
+            return serverPath;
+        }
+
+        @Override
+        public boolean isSuccessful() {
+            return true;
+        }
+    }
+
+    class Fail implements ServerCreationResult {
+
+        private final String reason;
+
+        public Fail(String reason) {
+            this.reason = reason;
+        }
+
+        public String getReason() {
+            return reason;
+        }
+
+        @Override
+        public boolean isSuccessful() {
+            return false;
+        }
+    }
+}
