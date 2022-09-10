5e171055a3a11d3c0ad92750662d6a12823e274b
funzter <funzter@mail.timesnake.de>
2022-09-10 20:45:26 +0200

    private, public servers

---
+++ src/main/java/de/timesnake/library/network/ServerInitResult.java
@@ -0,0 +1,44 @@
+package de.timesnake.library.network;
+
+import java.nio.file.Path;
+
+public interface ServerInitResult {
+
+    boolean isSuccessful();
+
+    class Successful implements ServerInitResult {
+
+        private final Path templatePath;
+
+        public Successful(Path templatePath) {
+            this.templatePath = templatePath;
+        }
+
+        public Path getTemplatePath() {
+            return templatePath;
+        }
+
+        @Override
+        public boolean isSuccessful() {
+            return true;
+        }
+    }
+
+    class Fail implements ServerInitResult {
+
+        private final String reason;
+
+        public Fail(String reason) {
+            this.reason = reason;
+        }
+
+        public String getReason() {
+            return reason;
+        }
+
+        @Override
+        public boolean isSuccessful() {
+            return false;
+        }
+    }
+}
