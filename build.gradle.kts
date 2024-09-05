1bb1f11f7205ae4e872a6e9c9b39d8701e19f1d4
funzter <funzter@mail.timesnake.de>
2024-09-06 00:53:24 +0200

    change gradle dsl to kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,75 @@
+plugins {
+    id("java")
+    id("java-base")
+    id("java-library")
+    id("maven-publish")
+}
+
+
+group = "de.timesnake"
+version = "2.0.0"
+var projectId = 37
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    maven {
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+    maven {
+        url = uri("https://git.timesnake.de/api/v4/groups/7/-/packages/maven")
+        name = "timesnake"
+        credentials(PasswordCredentials::class)
+    }
+}
+
+dependencies {
+    implementation("org.freemarker:freemarker:2.3.31")
+    implementation("commons-io:commons-io:2.11.0")
+
+    compileOnly("de.timesnake:database-api:3.+")
+    compileOnly("de.timesnake:channel-api:5.+")
+    compileOnly("de.timesnake:library-basic:2.+")
+
+    compileOnly("com.moandjiezana.toml:toml4j:0.7.3-SNAPSHOT")
+}
+
+configurations.configureEach {
+    resolutionStrategy.dependencySubstitution {
+        if (project.parent != null) {
+            substitute(module("de.timesnake:database-api")).using(project(":database:database-api"))
+            substitute(module("de.timesnake:channel-api")).using(project(":channel:channel-api"))
+            substitute(module("de.timesnake:library-basic")).using(project(":libraries:library-basic"))
+        }
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            url = uri("https://git.timesnake.de/api/v4/projects/$projectId/packages/maven")
+            name = "timesnake"
+            credentials(PasswordCredentials::class)
+        }
+    }
+
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
+}
+
+tasks {
+    compileJava {
+        options.encoding = "UTF-8"
+        options.release = 21
+    }
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+    withSourcesJar()
+}
